d62a17c4ae00dc54968b7a0173701a028c517f80
dirtTastesGood <keegood8@gmail.com>
2021-01-11 15:27:05 -0800

    added selection sort

---
+++ algorithms/sorting/python/selection_sort.py
@@ -0,0 +1,26 @@
+from SortAlgo import SortAlgo
+
+class SelectionSort(SortAlgo):
+    # selection sort - O(n^2) worst case
+    def sort(self):
+        nums = self.nums
+
+        minimum = nums[0] # current minimum
+        
+        k = 1 # first unsorted index
+
+        for i in range(len(nums) - 1):
+            # if the current number is less than the minimum
+            if nums[i] < minimum:
+                # the current number becomes the minimum
+                minimum = nums[i]
+
+        nums[k] = nums[i]
+        nums[i] = minimum
+
+    return nums
+
+
+if __name__ == '__main__':
+    s = SelectionSort()
+    print(s.sort())
