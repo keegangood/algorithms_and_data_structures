1e51449a6d721d52caaef94a71d8987a1b4c2f73
unknown <keegood8@gmail.com>
2021-01-25 16:23:47 -0800

    updated

--- algorithms/searching/python/binary_search.py
+++ algorithms/searching/python/binary_search.py
@@ -1,19 +1,22 @@
 from random import randint
 
+
 def binary_search(arr, target):
-    print(arr)
+    low = 0
+    high = len(arr) - 1
 
-    if len(arr) == 1 and arr[0
+    while low <= high:
+        mid = (low + high) // 2
+        
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        elif arr[mid] > target:
+            high = mid + 1
 
-    print(left, right, arr[mid], sep=" | ")
+    return None
+    
+nums =  sorted([randint(1, 100) for i in range(2000000)])#[3, 5, 8, 19, 24, 30, 30, 34, 40, 44, 50, 51, 65, 66, 69, 79, 82, 85, 89, 93, 95]
 
-    if arr[mid] == target:
-        print(mid)
-        return mid
-    elif arr[mid] < target:
-        binary_search(right, target)
-    elif arr[mid] > target:
-        binary_search(left, target)
-
-nums = sorted([randint(1, 100) for i in range(21)])
-print(binary_search(nums, 15))
+print(binary_search(nums, 10))
