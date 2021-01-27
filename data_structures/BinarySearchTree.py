f78b19fb3f5774f9105c65a23c3a1df6ab69eaf5
unknown <keegood8@gmail.com>
2021-01-26 22:43:49 -0800

    added search

---
+++ data_structures/BinarySearchTree.py
@@ -0,0 +1,88 @@
+class BSTNode:
+    def __init__(self, val):
+        self.val = val
+        self.parent = None
+        self.right = None
+        self.left = None
+        self.count = 1
+
+    def __repr__(self):
+        return str(self.val)
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+    
+    def _search(self, number, root):
+        if root is None:
+            return False
+
+        if number == root.val:
+            return root
+        
+        elif number > root.val:
+            return self._search(number, root.right)
+
+        elif number < root.val:
+            return self._search(number, root.left)
+        
+    def search(self, number):
+        return self._search(number, self.root)
+
+    def _insert(self, val, root):
+        '''recursively place the new value'''
+        if root is None:
+            node = BSTNode(val)
+            if self.root is None:
+                self.root = node
+            return node
+        
+        # if the value is equal to the root
+        if val == root.val:
+            root.count += 1
+            return root
+
+        # if the value is less than the root's value
+        elif val < root.val:
+            # if left node is empty
+            if root.left is None:
+                root.left = BSTNode(val)
+                return root.left
+
+            else:
+                # call _insert() on left child
+                self._insert(val, root.left)
+
+        elif val > root.val:
+            if root.right is None:
+                root.right = BSTNode(val)
+                return root.right
+            else:
+                # call _insert() on return child
+                self._insert(val, root.right)
+
+    def insert(self, val):
+        return self._insert(val, self.root)
+        
+    def delete(self, val):
+        pass
+
+
+if __name__ == '__main__':
+    from random import randint
+
+    bst = BinarySearchTree()
+
+    nums = [randint(1, 10) for i in range(10)]
+
+    for num in nums:
+        bst.insert(num)
+
+    val = 5
+    node = bst.search(val)
+    if node:
+        print(f'The value {val} exists in the BST {node.count} times')
+    else:
+        print(f'The value {val} does not exist in the BST')
+
+
