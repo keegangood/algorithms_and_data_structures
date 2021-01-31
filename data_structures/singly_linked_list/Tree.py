99dd737cdfc5197d2267091832a864121cb92b56
unknown <keegood8@gmail.com>
2021-01-30 17:22:28 -0800

    added preorder, postorder, inorder

--- data_structures/singly_linked_list/Tree.py
+++ data_structures/singly_linked_list/Tree.py
@@ -8,25 +8,58 @@ class Tree:
     def __init__(self):
         root = None
 
-    def preorder(self):
-        pass
+    def preorder(self, n):
+        '''left -> root -> right'''
+        if n is None:
+            return
 
-    def postorder(self):
-        pass
+        self.preorder(n.left)
 
-    def inorder(self):
-        pass
+        print(n.val, end=' ')
+
+        self.preorder(n.right)
+
+    def postorder(self, n):
+        '''right -> root -> left'''
+        if n is None:
+            return
+        
+        self.postorder(n.right)
+
+        print(n.val, end=' ')
+
+        self.postorder(n.left)
+
+    def inorder(self, n):
+        '''root -> left -> right'''
+        if n is None:
+            return
+        
+        print(n.val, end=' ')
+
+        self.inorder(n.left)
+
+        self.inorder(n.right)
 
 
 
 if __name__ == '__main__':
     t = Tree()
-    t.root = TreeNode(10)
+    t.root = TreeNode(4)
+
+    t.root.right = TreeNode(6) 
+    t.root.right.left = TreeNode(5)
+    t.root.right.right = TreeNode(7)
 
     t.root.left = TreeNode(2) 
-    t.root.right = TreeNode(3) 
-    t.root.left.left = TreeNode(4) 
-    t.root.left.right = TreeNode(5) 
+    t.root.left.left = TreeNode(1) 
+    t.root.left.right = TreeNode(3) 
 
+    print('\nPre-Order')
+    t.preorder(t.root)
+    
+    print('\nIn-Order')
+    t.inorder(t.root)
 
-    t.inorder()
+    print('\nPost-Order')
+    t.postorder(t.root)
